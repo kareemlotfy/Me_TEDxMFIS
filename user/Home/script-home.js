a4bf2b5e1b569f3b8f83c9c1dacf118e91f0e93e
Kareem Mohammed Lotfy <kareem.mohammed.lotfy@gmail.com>
2024-07-10 05:40:02 +0300

    Schedule Responsive  V2

--- user/Home/script-home.js
+++ user/Home/script-home.js
@@ -1,18 +1,88 @@
-// Make The Team Section (top=-100px)
+// JS Code With Slick 
+// Code For Making Slide To Schedule Container
+$(document).ready(function () {
+    $('.schedule_container').slick({
+      infinite: false,
+      slidesToShow: 5,
+      slidesToScroll: 0,
+      autoplay: false,
+      // autoplaySpeed: 2000,
+      responsive: [{
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            infinite: false,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 799,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 479,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    });
+  });
 
-// window.addEventListener('DOMContentLoaded', (event) => {
-//     // Adjust the top position of the section with id=team
-//     const teamSection = document.getElementById('team');
-//     teamSection.style.marginTop = '-100px';
-//   });
+// Code to Resize Schedule Container and Card 
+// From Screen Size 780px to 767px
+function adjustContainerWidths() {
+  const windowWidth = window.innerWidth;
+  const scheduleContainer = document.querySelector('.schedule_container');
+  const scheduleCard = document.querySelectorAll('.schedule_card');
 
-// Event Date Countdown Start
-// 1000 millisecond = 1 second
+  if (windowWidth >= 767 && windowWidth <= 780) {
+    const diff = 780 - windowWidth;
+    scheduleContainer.style.width = (780 - diff) + 'px';
+    scheduleCard.forEach(card => {
+      card.style.width = (330 - 2 * diff) + 'px';
+    });
+  }
+  else if (windowWidth >= 480 && windowWidth <= 600) {
+    const diff = 600 - windowWidth;
+    scheduleContainer.style.width = (570 - diff) + 'px';
+    scheduleCard.forEach(card => {
+      card.style.width = (225 - 2 * diff) + 'px';
+    });
+  }
+  else if(windowWidth <= 480) {
+    const diff = 480 - windowWidth;
+    scheduleContainer.style.width = (450 - diff) + 'px';
+  }
+  else{
+    return
+  }
+}
 
-let countDownDate = new Date("Nov 15, 2024 14:00:00").getTime();
+window.addEventListener('resize', adjustContainerWidths);
+window.addEventListener('load', adjustContainerWidths);
 
 
-let counter = setInterval(() => {
+  // Make The Team Section (top=-100px)
+
+  // window.addEventListener('DOMContentLoaded', (event) => {
+  //     // Adjust the top position of the section with id=team
+  //     const teamSection = document.getElementById('team');
+  //     teamSection.style.marginTop = '-100px';
+  //   });
+
+  // Event Date Countdown Start
+  // 1000 millisecond = 1 second
+
+  let countDownDate = new Date("Nov 15, 2024 14:00:00").getTime();
+
+
+  let counter = setInterval(() => {
     // Get Date Now 
     let dateNow = new Date().getTime();
 
@@ -22,23 +92,23 @@ let counter = setInterval(() => {
     // Get Time Units
     // let days = Math.floor(dateDiff / 1000 / 60 / 60 / 24);
     let days = Math.floor(dateDiff / (1000 * 60 * 60 * 24));
-    let hours = Math.floor((dateDiff % (1000 * 60 * 60 * 24))  / (1000 * 60 * 60));
-    let minutes = Math.floor((dateDiff % (1000 * 60 * 60))  / (1000 * 60));
-    let seconds = Math.floor((dateDiff % (1000 * 60 ))  / 1000);
+    let hours = Math.floor((dateDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((dateDiff % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((dateDiff % (1000 * 60)) / 1000);
 
     document.querySelector(".days").innerHTML = days < 10 ? `0${days}` : days;
     document.querySelector(".hours").innerHTML = hours < 10 ? `0${hours}` : hours;
     document.querySelector(".minutes").innerHTML = minutes < 10 ? `0${minutes}` : minutes;
     document.querySelector(".seconds").innerHTML = seconds < 10 ? `0${seconds}` : seconds;
 
-    if (dateDiff < 0){
-        clearInterval(counter);
+    if (dateDiff < 0) {
+      clearInterval(counter);
     }
-}, 1000)
-// Event Date Countdown End
+  }, 1000)
+  // Event Date Countdown End
 
-// Function to play the video Start
-function playVideo() {
+  // Function to play the video Start
+  function playVideo() {
     var video = document.getElementById('myVideo');
     var playButton = document.querySelector('.play-button');
 
@@ -48,83 +118,82 @@ function playVideo() {
     // Display and play the video
     video.style.display = 'block';
     video.play();
-}
-// Function to play the video End
+  }
+  // Function to play the video End
 
 
-// Function for the animation of home hero text
-// document.addEventListener('DOMContentLoaded', function() {
-//     const flyingTexts = document.querySelectorAll('.flying-text');
-  
-//     flyingTexts.forEach((text, index) => {
-//       setTimeout(() => {
-//         text.style.animationName = 'flyIn';
-//         text.style.animationDelay = index * 0.5 + 's';
-//       }, 600); // Delay initial animation for smoother loading
-//     });
-//   });
+  // Function for the animation of home hero text
+  // document.addEventListener('DOMContentLoaded', function() {
+  //     const flyingTexts = document.querySelectorAll('.flying-text');
+
+  //     flyingTexts.forEach((text, index) => {
+  //       setTimeout(() => {
+  //         text.style.animationName = 'flyIn';
+  //         text.style.animationDelay = index * 0.5 + 's';
+  //       }, 600); // Delay initial animation for smoother loading
+  //     });
+  //   });
 
 
-// Set padding of class="cards"
+  // Set padding of class="cards"
 
-function setDynamicPadding() {
-  var windowWidth = window.innerWidth;
+  function setDynamicPadding() {
+    var windowWidth = window.innerWidth;
 
-  // Check if window width is less than or equal to 600px
-  if (windowWidth <= 600) {
-    // Remove inline styles added by JavaScript
+    // Check if window width is less than or equal to 600px
+    if (windowWidth <= 600) {
+      // Remove inline styles added by JavaScript
+      var elements = document.querySelectorAll('.cards');
+      elements.forEach(function (element) {
+        element.style.removeProperty('padding-left');
+        element.style.removeProperty('padding-right');
+      });
+      return; // Exit the function
+    }
+
+    var mainContentWidth = document.querySelector('.inner_content').offsetWidth;
+    var paddingValue = windowWidth - mainContentWidth;
+    var paddingLR = paddingValue / 2;
+
+    // Apply padding to the desired class
     var elements = document.querySelectorAll('.cards');
-    elements.forEach(function(element) {
-      element.style.removeProperty('padding-left');
-      element.style.removeProperty('padding-right');
+    elements.forEach(function (element) {
+      element.style.paddingLeft = paddingLR + 'px';
+      element.style.paddingRight = paddingLR + 'px';
     });
-    return; // Exit the function
   }
 
-  var mainContentWidth = document.querySelector('.inner_content').offsetWidth;
-  var paddingValue = windowWidth - mainContentWidth;
-  var paddingLR = paddingValue / 2;
-
-  // Apply padding to the desired class
-  var elements = document.querySelectorAll('.cards');
-  elements.forEach(function(element) {
-    element.style.paddingLeft = paddingLR + 'px';
-    element.style.paddingRight = paddingLR + 'px';
-  });
-}
-
-// Call the function initially and on window resize
-window.onload = setDynamicPadding;
-window.addEventListener('resize', setDynamicPadding);
+  // Call the function initially and on window resize
+  window.onload = setDynamicPadding;
+  window.addEventListener('resize', setDynamicPadding);
 
 
 
 
-function adjustPhotoSize() {
+  function adjustPhotoSize() {
     var screenWidth = window.innerWidth;
 
     // Check if the screen width is 600 pixels or less
     if (screenWidth <= 600) {
-        var initialWidth = 520; 
-        var initialHeight = 500; 
+      var initialWidth = 520;
+      var initialHeight = 500;
 
-        // Calculate the new width and height based on the formula
-        var newWidth = initialWidth - (600 - screenWidth) * 0.91;
-        var newHeight = initialHeight - (600 - screenWidth) * 0.84;
+      // Calculate the new width and height based on the formula
+      var newWidth = initialWidth - (600 - screenWidth) * 0.91;
+      var newHeight = initialHeight - (600 - screenWidth) * 0.84;
 
-        // Apply the new width and height to your photo element
-        document.getElementById("abt_img").style.width = newWidth + "px";
-        document.getElementById("abt_img").style.height = newHeight + "px";
-    }else{
+      // Apply the new width and height to your photo element
+      document.getElementById("abt_img").style.width = newWidth + "px";
+      document.getElementById("abt_img").style.height = newHeight + "px";
+    } else {
       return
     }
-}
+  }
 
-// Call the function after the DOM is fully loaded
-document.addEventListener("DOMContentLoaded", function() {
+  // Call the function after the DOM is fully loaded
+  document.addEventListener("DOMContentLoaded", function () {
     adjustPhotoSize();
-});
+  });
 
-// Listen for the window resize event and call the function
-window.addEventListener('resize', adjustPhotoSize);
-
+  // Listen for the window resize event and call the function
+  window.addEventListener('resize', adjustPhotoSize);
