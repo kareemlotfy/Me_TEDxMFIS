17e4106bc23deb97c328f2c6ae133dd8c97a3e66
Kareem Mohammed <kareem.mohammed.lotfy@gmail.com>
2025-10-12 20:55:05 +0300

    Update db_conn.php

--- Misc/db_conn.php
+++ Misc/db_conn.php
@@ -1,10 +1,9 @@
 <?php 
 
 $hname = 'localhost';   // e.g., 'localhost' or 'mysql.hostingprovider.com'
-// $hname = 'srv1687.hstgr.io';   // e.g., 'localhost' or 'mysql.hostingprovider.com'
-$uname = 'u654105294_tedxmfisun';//u654105294_tedxmfisun
-$pass = 'TEDxMFIS@DB@Password1';//TEDxMFIS@DB@Password1
-$db = 'u654105294_tedxmfis';//u654105294_tedxmfis
+$uname = 'root'; //u654105294_tedxmfisun
+$pass = ''; //TEDxMFIS@DB@Password1
+$db = 'tedx'; //u654105294_tedxmfis
 
 $con = mysqli_connect($hname, $uname, $pass, $db);
 
